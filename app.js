4b1efa87d2b9cdc7dcf84b98181a64d12fcbe4f4
David Harris <git@dsharris.org>
2014-08-31 21:53:23 -0500

    built

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express'),
+    path = require('path'),
+    favicon = require('static-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    routes = require('./routes/index'),
+    users = require('./routes/users'),
+    app = express(),
+    fs = require("fs"),
+    server;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', function (req, res) {
+    fs.readFile('./views/resume.ejs', function (err, resume) {
+        res.end(resume.toString());
+    });
+});
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+server = app.listen(7378, function() {});
+
+
+module.exports = app;
---
+++ views/resume.md
@@ -0,0 +1,50 @@
+# David S. Harris
+
+## Skills
+
+- Capible of running a project from post-design through production
+- Effective Team Memeber
+- Effective at running a project solo
+- Capable of working on projects from brochure sites through full scale applications
+- Modular development
+- Use and creation of automation tool
+
+## Previous Employment
+
+### 40Digits
+#### Software Engineer
+#### 5-12 - Current
+Hired in Springfield Missouri as the 8 employee in the office, i have worked for 40Digits as a software engineer for nearly 2 and a half years. While with the company, i have worked primarily in a backend capacity. On sites ranging from simple wordpress sites, to full blown employee portals. Most projects where team efforts but i did work several on my own.
+
+### yTen
+#### Software Engineer
+#### 5-12 - Current
+Hired in Springfield Missouri as the 8 employee in the office, i have worked for 40Digits as a software engineer for nearly 2 and a half years. While with the company, i have worked primarily in a backend capacity. On sites ranging from simple wordpress sites, to full blown employee portals. Most projects where team efforts but i did work several on my own.
+
+
+## Languages, Frameworks & Modules
+
+- Javascript / NodeJS
+	- Express
+	- Underscore
+	- Backbone
+	- jQuery
+	- Hammer.js
+	- Moment.js
+	- Mongoose
+	- Grunt
+	- Gulp
+	- ...
+- PHP
+	- Laravel
+	- Codeigniter
+	- Wordpress
+	- Drupal
+	- mPDF
+- Coldfusion
+	- All hand written code
+- Languate Agnostic Tools
+	- MongoDB
+	- Neo4j
+	- Redis
+	- Git
